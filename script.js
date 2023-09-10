08e2e7846108b271c53b3bf86e4bbc87c61a4224
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2023-09-10 13:47:10 +0100

    Implemented the hover effect for each of the divs

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // grid.classList.add("div-group");
 const container = document.querySelector(".container");
 
-for (let i = 1; i < 17; i++) {
+for (let i = 1; i < 16; i++) {
   const grid = document.createElement("div");
   grid.classList.add(`box`);
   container.appendChild(grid);
@@ -10,9 +10,20 @@ for (let i = 1; i < 17; i++) {
 
 const grids = document.querySelectorAll("div");
 grids.forEach((grid) => {
-  for (let i = 1; i < 17; i++) {
+  for (let i = 1; i < 16; i++) {
     const div = document.createElement("div");
     div.classList.add("inner");
     grid.appendChild(div);
   }
 });
+
+const divs = document.querySelectorAll(".inner");
+console.log(divs);
+
+divs.forEach((div) => {
+  div.addEventListener("mouseover", () => {
+    div.classList.add("black");
+  });
+});
+
+
